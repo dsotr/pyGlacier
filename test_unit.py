fe9e0c530596b4a388685e20f125da41d07def8f
Daniele <dbiscr@gmail.com>
2015-04-13 18:01:07 +0200

    multiupload still screwed

--- test_unit.py
+++ test_unit.py
@@ -6,19 +6,20 @@ from aws_libs import *
 from subprocess import Popen, PIPE
 
 
-class TestTree_hash(TestCase):
-    def test_tree_hash(self):
-        # file_path = "Downloads.tar"
-        # out_j = Popen(['java', 'TreeHashExample', file_path], stdout=PIPE)
-        # out_p = tree_hash(file_path, 0, os.path.getsize(file_path))
-        # self.assertEqual(out_j.stdout.read().decode(), bytes_to_hex(out_p))
-        print("PASSED: tree hash")
+# class TestTree_hash(TestCase):
+#     def test_tree_hash(self):
+#         # file_path = "Downloads.tar"
+#         # out_j = Popen(['java', 'TreeHashExample', file_path], stdout=PIPE)
+#         # out_p = tree_hash(file_path, 0, os.path.getsize(file_path))
+#         # self.assertEqual(out_j.stdout.read().decode(), bytes_to_hex(out_p))
+#         print("PASSED: tree hash")
 
 class TestGlacierClient(TestCase):
     def test_multiupload_archive(self):
         c = GlacierClient(debug=True)
-        file_path = "Downloads.tar"
+        file_path = "testupload.txt"
         response = c.multiupload_archive('Foto', file_path)
+        self.assertIsNotNone(response)
         self.assertIn('x-amz-multipart-upload-id', response.headers)
         self.assertIn('Location', response.headers)
         print("PASSED: Multiupload archive")
